d09756528b96fa6eba14a97394ed4f5daebba501
Maxim Grushevsky <m.grushevsky10@gmail.com>
2017-09-18 15:43:47 +0300

    Initial commit

---
+++ WpfApp2/ReportPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for ReportPage.xaml
+    /// </summary>
+    public partial class ReportPage : Page
+    {
+        public ReportPage()
+        {
+            InitializeComponent();
+        }
+
+        public ReportPage(object data):this()
+        {
+            this.DataContext = data;
+        }
+    }
+}
